494772ca60daf62ef5adf02c408e0039b8c0a43a
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-03-23 17:45:02 +0100

    Created Add Post Screen

---
+++ src/screens/AddPostScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function AddPostScreen() {
+  return (
+    <View style={{ backgroundColor: "#000", padding: 20, flex: 1}}>
+      <Text style={{color: "#fff"}}>Add your post here...</Text>
+    </View>
+  )
+}
+
+export default AddPostScreen
